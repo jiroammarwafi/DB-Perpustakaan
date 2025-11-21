a894f64e306937089ea02d005dcaa3ad13faba6a
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2025-11-21 11:59:42 +0700

    Fixed testbackend (error sebelumnya karena id foreign key 27 dan 28 milik kategori tidak ada (nonexistent) dalam database sehingga muncul error

---
+++ src/frontend/TestBackend.java
@@ -0,0 +1,40 @@
+package src.frontend;
+import src.backend.*;
+
+public class TestBackend {
+    public static void main(String[] args) {
+        Kategori novel = new Kategori().getById(27);
+        if (novel.getIdKategori() == 0) {
+            // If category doesn't exist, create it for testing
+            novel = new Kategori("Novel", "Kategori Novel (auto-created)");
+            novel.save();
+        }
+
+        Kategori referensi = new Kategori().getById(28);
+        if (referensi.getIdKategori() == 0) {
+            referensi = new Kategori("Referensi", "Kategori Referensi (auto-created)");
+            referensi.save();
+        }
+
+        Buku buku1 = new Buku(novel, "Timun Mas", "Elex Media", "Bang Supit");
+        Buku buku2 = new Buku(referensi, "Metode Linier", "Springer", "Alex Baldwin");
+        Buku buku3 = new Buku(novel, "Bintang Terang", "Erlangga", "Mat Sewoot");
+
+        // test insert
+        buku1.save();
+        buku2.save();
+        // test update
+        buku2.setJudul("Aljabar Linier");
+        buku2.save();
+        // test delete
+        buku3.delete();
+        // test select all
+        for(Buku b : new Buku().getAll()) {
+            System.out.println("Kategori: " + b.getKategori().getNama() + ", Judul: " + b.getJudul());
+        }
+        // test search
+        for(Buku b : new Buku().search("timun")) {
+            System.out.println("Kategori: " + b.getKategori().getNama() + ", Judul: " + b.getJudul());
+        }
+    }
+}
